b67a4b736004a7e3ad20c2a6feea7c09006a867e
ihgazni <terryinzaghi@163.com>
2018-06-14 10:51:46 +0800

    init.sh

---
+++ init.sh
@@ -0,0 +1,9 @@
+git init
+git remote add origin https://github.com/ihgazni/rdragon.git
+git pull origin master
+
+git config --global user.email "terryinzaghi@163.com"
+git config --global user.email "terryinzaghi@163.com"
+
+git add .
+git commit -m "init.sh"

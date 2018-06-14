e39129232011329db4c204bee00b26177d7bd0aa
ihgazni <terryinzaghi@163.com>
2018-06-14 11:04:35 +0800

    init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,22 @@
+username = $1
+password = $2
+
+email = $3
+commituser = $4
+
+reponame = rdragon
+
+echo https://${username}:${password}@github.com  > $HOME/.git-credentials
+
 git init
-git remote add origin https://github.com/ihgazni/rdragon.git
+git remote add origin https://github.com/${username}/${reponame}.git
 git pull origin master
 
-git config --global user.email "terryinzaghi@163.com"
-git config --global user.email "terryinzaghi@163.com"
+
+git config --global user.email ${email}
+git config --global user.name ${commituser}
+git config --global credential.helper store
 
 git add .
 git commit -m "init.sh"
+git push origin master
